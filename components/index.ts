89a1b6cdc2197bcf749240301d5bb56b29b0fc44
adegoke bestman <adegokebestman@gmail.com>
2023-07-20 17:17:01 +0100

    Car search box init

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,15 @@ import Hero from "./Hero";
 import CustomButton from './CustomButton';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import Searchbar from "./Searchbar";
+import SearchManufacturer from "./SearchManufacturer";
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    CustomFilter,
+    Searchbar,
+    SearchManufacturer
 }
